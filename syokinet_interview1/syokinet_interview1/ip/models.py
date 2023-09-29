cb68d1ebfa0152a749a58836fc4839a9e3dac376
Perez Githumbi <codakniggz145@gmail.com>
2023-09-29 12:31:26 +0300

    created models for ip addresses and customers

--- syokinet_interview1/syokinet_interview1/ip/models.py
+++ syokinet_interview1/syokinet_interview1/ip/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    customer_first_name = models.CharField(max_length=100)
+    customer_last_name = models.CharField(max_length=100)
+
+class Ips(models.Model):
+    AVAILABLE = 'available'
+    ALLOCATED = 'allocated'
+    RESERVED = 'reserved'
+    STATUSES = ((AVAILABLE, AVAILABLE,), 
+                (ALLOCATED, ALLOCATED), 
+                (RESERVED, RESERVED))
+
+    address = models.GenericIPAddressField()
+    status = models.CharField(max_length=20, choices=STATUSES, 
+                              default=AVAILABLE)
+    customer = models.OneToOneField(Customer, null=True, default=None,
+                                    on_delete=models.SET_NULL)
